c93461855136007f43e502f2fc5d4ede2a7dae93
/Faisalahammed003 <faisalahammed5035@gmail.com>
2025-02-26 21:36:17 +0600

    url-add

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://task-management00.web.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
